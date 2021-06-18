238f7768e86ee0cf7470b82da997ac7f83cd54fa
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2021-06-17 23:16:06 -0500

    use ajax for form submission and setup javaScript response

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,10 +3,13 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render "users/my_portfolio"
+        respond_to do |format|
+          format.js { render partial: "users/result" }
+        end
+      else
+        flash[:alert] = "Please enter a valid symbol to search"
+        redirect_to my_portfolio_path
       end
-      flash[:alert] = "Please enter a valid symbol to search"
-      redirect_to my_portfolio_path
     else
       flash[:alert] = "Please enter a symbol to search"
       redirect_to my_portfolio_path
