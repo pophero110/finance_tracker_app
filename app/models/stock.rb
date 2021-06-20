987967ab6fe95199cf4c841cbf54a5d79e176ce4
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2021-06-20 18:12:29 -0500

    track stocks from front-end browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,9 +8,12 @@ class Stock < ApplicationRecord
       endpoint: "https://sandbox.iexapis.com/v1",
     )
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      new(ticker: ticker_symbol.upcase, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
       return nil
     end
+  end
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
