4808403ffa51b07281542a29d9ea2584813bdb83
pophero110 <38047622+pophero110@users.noreply.github.com>
2021-06-25 18:59:08 -0500

    fix redirect loop

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create, :destroy]
-  resources :friendships, only: [:create, :destroy]
-  resources :users, only: [:show]
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#index"
   get "my_portfolio", to: "users#my_portfolio"
   get "friends", to: "users#friends"
   get "search_friend", to: "users#search"
   get "search_stock", to: "stocks#search"
+  resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
